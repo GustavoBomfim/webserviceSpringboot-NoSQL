cbdabd156d3e11431c207caf42d384286feb8ec2
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-25 13:30:13 -0300

    Inserção de usuario com post

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,9 +1,11 @@
 package service;
 
 import domain.User;
+import dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
+import service.exception.ObjectNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +22,12 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
