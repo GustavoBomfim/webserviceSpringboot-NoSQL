c4ebd686506b9434765793f8203b64bc4d584411
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-25 10:52:36 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/config/Instantiation.java
@@ -0,0 +1,26 @@
+package config;
+
+import domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import repository.UserRepository;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... arg0) throws Exception{
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
