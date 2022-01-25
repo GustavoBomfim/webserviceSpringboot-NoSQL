599d2a949e778ef62ed33180e4eac0397a65f96f
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-25 08:43:05 -0300

    Entity user

---
+++ src/main/java/springboot_nosql/gustavobomfim/GustavobomfimApplication.java
@@ -0,0 +1,13 @@
+package springboot_nosql.gustavobomfim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustavobomfimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GustavobomfimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot_nosql/gustavobomfim/GustavobomfimApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_nosql.gustavobomfim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavobomfimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
